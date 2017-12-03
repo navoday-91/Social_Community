2d101cd58e57ffaf9f3767dfaf5a188e742f85da
navoday-91 <navoday.91@gmail.com>
2017-12-03 14:18:25 -0800

    CMPE281

--- testscript.py
+++ testscript.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -u
 
 import sys, time
 # Turn on debug mode.
@@ -7,8 +7,8 @@ cgitb.enable()
 # Print necessary headers.
 print("Content-Type: text/html")
 print()
-print("Community requested is : {}\n".format(' '.join(sys.argv[1])))
+print("Community requested is : {}\n".format(' '.join(sys.argv)))
 sys.stdout.flush()
-time.sleep(15)
-print("Community requested is : {}".format(' '.join(sys.argv[1])))
+time.sleep(5)
+print("Community requested is : {}".format(' '.join(sys.argv)))
 sys.stdout.flush()
