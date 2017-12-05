65cb9b88c3ffcf7c3e1f9bbde7fddc91564d0cdf
navoday-91 <navoday.91@gmail.com>
2017-12-05 12:45:00 -0800

    CMPE281

--- php/cmpe281login.php
+++ php/cmpe281login.php
@@ -1,6 +1,6 @@
 <?php
 session_start(); // Starting Session
-$community = $_SESSION['community'];
+$community = $_POST['community'];
 $error=''; // Variable To Store Error Message
 if (isset($_POST['Login'])) {
 if (empty($_POST['user_username']) || empty($_POST['user_password'])) {
@@ -34,8 +34,9 @@ if ($connection->connect_error) {
 }
 echo("Connected successfully \n");
 $db = mysqli_select_db($connection, "cmpe281");
+$query = mysqli_query($connection, "select * from community_details where comm_name = '$community';");
 // SQL query to fetch information of registerd users and finds user match.
-$query = mysqli_query($connection, "select * from community_details where comm_name = $_POST['community'];");
+$rows = mysqli_num_rows($query);
 // To protect MySQL injection for Security purpose
 if ($rows == 1) {
     while ($user = $query->fetch_assoc()) {
