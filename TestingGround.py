5cad0749746ae4c68c7997bda9f19999c7892c6b
navoday-91 <navoday.91@gmail.com>
2017-12-05 10:14:36 -0800

    CMPE281

--- TestingGround.py
+++ TestingGround.py
@@ -129,7 +129,7 @@ try:
 except:
     print('failed')
 
-sql = "create table login(`username` varchar(30), `password` varchar(20), `community_name` varchar(20);"
+sql = "create table login(`username` varchar(30), `password` varchar(20), `community_name` varchar(20));"
 print(sql)
 
 try:
@@ -138,7 +138,7 @@ except:
     print("Error Here2")
 
 sql = "create table userdata(`username` varchar(30), `first name` varchar(20), `last name` varchar(20), `email` " \
-      "varchar(50), `address` varchar(50), `phone` varchar(20), `community` varchar(40), `picurl` varchar(300);"
+      "varchar(50), `address` varchar(50), `phone` varchar(20), `community` varchar(40), `picurl` varchar(300));"
 print(sql)
 
 try:
@@ -146,7 +146,13 @@ try:
 except:
     print("Error Here3")
 
+sql = "create table groups(`groupname` varchar(30), `grouptype` varchar(5), `community` varchar(40));"
+print(sql)
 
+try:
+    cursor.execute(sql)
+except:
+    print("Error Here3")
 db.commit()
 
 
