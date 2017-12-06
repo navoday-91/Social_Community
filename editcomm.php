51f8a66555c18b2f70ee858006cc02714a55bee4
navoday-91 <navoday.91@gmail.com>
2017-12-05 18:47:50 -0800

    CMPE281

--- editcomm.php
+++ editcomm.php
@@ -447,7 +447,7 @@ if ($connection->connect_error) {
                                                 }
                                                 $db = mysqli_select_db($connection, "cmpe281");
                                                 // SQL query to fetch information of registerd users and finds user match.
-                                                $query = mysqli_query($connection, "select * from community_details where comm_name = $commname;");
+                                                $query = mysqli_query($connection, "select * from community_details where comm_name = '$commname';");
                                                 // To protect MySQL injection for Security purpose
                                                 $rows = mysqli_num_rows($query);
                                                 if ($rows == 1) {
